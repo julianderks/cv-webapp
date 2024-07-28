8e117c5b5dd9f0de0d7135e04a4c3ce0f2dfcc45
Julian Derks <derks_julian@hotmail.com>
2024-07-28 11:34:38 +0200

    static adapter

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -13,10 +13,9 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		alias: {
-			"@/*": "./path/to/lib/*",
-		  },
+			'@/*': './path/to/lib/*'
+		}
 	}
 };
 
 export default config;
-
